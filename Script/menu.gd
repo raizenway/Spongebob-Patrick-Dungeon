1695e84e1b999a91ff381c350de2ce37676ac06a
Banteng Harisantoso <117459151+raizenway@users.noreply.github.com>
2024-06-08 13:08:16 +0700

    benerin folder

---
+++ Script/menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_play_spongebob_pressed():
+	get_tree().change_scene_to_file("res://Scene/map-spongebob.tscn")
+
+func _on_play_patrick_pressed():
+	get_tree().change_scene_to_file("res://Scene/map-patrick.tscn")
+
+
+func _on_exit_pressed():
+	get_tree().quit()

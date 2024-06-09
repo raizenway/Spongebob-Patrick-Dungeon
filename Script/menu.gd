c8719abc9aa13be58223427f7d8cf41d77668370
Banteng Harisantoso <117459151+raizenway@users.noreply.github.com>
2024-06-09 23:19:38 +0700

    backup, ada mob (ga kepake) sama fundamental prolog

--- Script/menu.gd
+++ Script/menu.gd
@@ -1,10 +1,10 @@
 extends Control
 
 func _on_play_spongebob_pressed():
-	get_tree().change_scene_to_file("res://Scene/map-spongebob.tscn")
+	get_tree().change_scene_to_file("res://Scene/prolog/prolog-1-spongebob.tscn")
 
 func _on_play_patrick_pressed():
-	get_tree().change_scene_to_file("res://Scene/map-patrick.tscn")
+	get_tree().change_scene_to_file("res://Scene/map-spongebob.tscn")
 
 
 func _on_exit_pressed():
